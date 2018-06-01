8de6d0cd178e21a5c27032a3029e0da63ff051dd
Hiei <james.illius@yahoo.com>
2018-06-01 17:56:37 -0400

    added comments

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,29 +1,33 @@
 $(document).ready(function () {
 
-
+    //needed to make a gloabl variable to avoid error at start. this is where the images and ratings are placed
     var animalDiv = $("<div>");
 
 
-
+    //on click function
     $("#animalSubmit").on("click", function () {
+        //checks if the user entered anything, if not alerts user, does not make button.
         if ($("#userInputText").val() === '') {
             event.preventDefault();
             alert("Not a valid submission");
         }
         else {
             event.preventDefault();
+            //takes the user's search and names the button 
             userSearch = $("#userInputText").val();
-
+            // api querurl
             var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userSearch + "&api_key=I3LIxn3wdbBKTNOyOvfS4UCk3TvTUwrY";
+            // makes the new button
             newAnimalButton = $("<button>" + userSearch + "</button>");
             newAnimalButton.addClass("animalClass");
             $("#newButtonArea").append(newAnimalButton);
+            // resets the user input box to blank after submit
             $("#userInputText").val('');
 
         }
 
 
-
+        // on click function for the created animal buttons
         $(newAnimalButton).on("click", function () {
 
 
@@ -31,16 +35,19 @@ $(document).ready(function () {
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
-
+                //clears the div when clicking another button 
                 animalDiv.empty();
+                //loop for all the 25 images to display
                 for (var i = 0; i < 25; i++) {
                     var displayStillImageUrl = response.data[i].images.fixed_height_small_still.url;
                     var displayGifImageUrl = response.data[i].images.fixed_height_small.url;
                     var rating = response.data[i].rating;
                     var image = $("<img>");
                     image.attr({ "src": displayStillImageUrl, "gif": displayGifImageUrl, "still": displayStillImageUrl });
+                    //used to switch between still and gif image
                     image.addClass("still");
                     image.addClass("animalImageClass");
+                    //switches between still image and gif
                     $(image).on("click", function () {
 
                         if ($(this).hasClass("still")) {
@@ -55,9 +62,11 @@ $(document).ready(function () {
                         }
 
                     });
+                    // puts the div and images inside of an html div
                     $("#searchResultArea").append(animalDiv);
-                    animalDiv.append(rating);
+                    animalDiv.append("Rating:" + rating);
                     animalDiv.append(image);
+
 
                 };
 
