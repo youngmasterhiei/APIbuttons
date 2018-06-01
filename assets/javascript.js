25bfe2c27367043cd52af411c66e901110d8db84
Hiei <james.illius@yahoo.com>
2018-06-01 16:20:51 -0400

    working pause on images

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,56 +1,70 @@
 $(document).ready(function () {
 
-    
+
     var animalDiv = $("<div>");
 
 
 
     $("#animalSubmit").on("click", function () {
-        if ($("#userInputText").val()=== ''){
+        if ($("#userInputText").val() === '') {
             event.preventDefault();
             alert("Not a valid submission");
         }
         else {
-        event.preventDefault();
-        userSearch = $("#userInputText").val();
-        
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userSearch + "&api_key=I3LIxn3wdbBKTNOyOvfS4UCk3TvTUwrY";
-        newAnimalButton = $("<button>" + userSearch + "</button>");
-        newAnimalButton.addClass("animalClass");
-        $("#newButtonArea").append(newAnimalButton);
-        $("#userInputText").val('');
-  
+            event.preventDefault();
+            userSearch = $("#userInputText").val();
+
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userSearch + "&api_key=I3LIxn3wdbBKTNOyOvfS4UCk3TvTUwrY";
+            newAnimalButton = $("<button>" + userSearch + "</button>");
+            newAnimalButton.addClass("animalClass");
+            $("#newButtonArea").append(newAnimalButton);
+            $("#userInputText").val('');
+
         }
-        
+
 
 
         $(newAnimalButton).on("click", function () {
 
-          
-                $.ajax({
-                    url: queryURL,
-                    method: "GET"
-                }).then(function (response) {
-                    console.log(response);
-                   
-                    animalDiv.empty();
-                    for (var i = 0; i < 25; i++) {
-                        var displayImageUrl = response.data[i].images.fixed_height_small.url;
-                        console.log(displayImageUrl);
-                        var image = $("<img>");
-                        image.attr("src", displayImageUrl);
 
-                        
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
 
-                        $("#searchResultArea").append(animalDiv);
-                        animalDiv.append(image);
-                        
-                    };
+                animalDiv.empty();
+                for (var i = 0; i < 25; i++) {
+                    var displayStillImageUrl = response.data[i].images.fixed_height_small_still.url;
+                    var displayGifImageUrl = response.data[i].images.fixed_height_small.url;
+                    var rating = response.data[i].rating;
+                    var image = $("<img>");
+                    image.attr({ "src": displayStillImageUrl, "gif": displayGifImageUrl, "still": displayStillImageUrl });
+                    image.addClass("still");
+                    image.addClass("animalImageClass");
+                    $(image).on("click", function () {
 
-                });
-            
-           
-    });
+                        if ($(this).hasClass("still")) {
+
+                            $(this).attr("src", $(this).attr("gif"));
+                            $(this).removeClass("still");
+
+                        }
+                        else {
+                            $(this).attr("src", $(this).attr("still"));
+                            $(this).addClass("still");
+                        }
+
+                    });
+                    $("#searchResultArea").append(animalDiv);
+                    animalDiv.append(rating);
+                    animalDiv.append(image);
+
+                };
+
+            });
+
+
+        });
 
     });
 
