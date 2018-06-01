cf31e9abb8952c74f1a29dc45921f9f6db19a1fe
Hiei <james.illius@yahoo.com>
2018-06-01 11:22:51 -0400

    working buttons and display of gifs, removes non correct animal gifs

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,46 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var animalArray = [];
-    var newAnimalButton;
-
-   
-
-
-$("#animalSubmit").on("click", function(){
-    event.preventDefault();
-    userSearch = $("#userInputText").val();
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userSearch + "&api_key=I3LIxn3wdbBKTNOyOvfS4UCk3TvTUwrY";
-     newAnimalButton = $("<button>" + userSearch + "</button>");
-    newAnimalButton.addClass("animalClass");
     
+    
+  
+    var animalDiv = $("<div>");
 
 
-$("#newButtonArea").append(newAnimalButton);
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(userSearch) {
-    console.log(userSearch);
-    
-    console.log(userSearch.data[0].embed_url);
-   
-    var displayImageUrl = userSearch.data[0].embed_url;
-    console.log(displayImageUrl);
-    var image = $("<img>").attr("src", displayImageUrl);
-    
-    $("#searchResultArea").append(image);
+
+    $("#animalSubmit").on("click", function () {
+        event.preventDefault();
+        userSearch = $("#userInputText").val();
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userSearch + "&api_key=I3LIxn3wdbBKTNOyOvfS4UCk3TvTUwrY";
+        newAnimalButton = $("<button>" + userSearch + "</button>");
+        newAnimalButton.addClass("animalClass");
+        $("#newButtonArea").append(newAnimalButton);
+
+
+        $(newAnimalButton).on("click", function () {
+
+          
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function (response) {
+                    console.log(response);
+                    animalDiv.empty();
+                    for (var i = 0; i < 25; i++) {
+                        var displayImageUrl = response.data[i].images.fixed_height_small.url;
+                        console.log(displayImageUrl);
+                        var image = $("<img>");
+                        image.attr("src", displayImageUrl);
+
+                        
+
+                        $("#searchResultArea").append(animalDiv);
+                        animalDiv.append(image);
+                        
+                    };
+
+                });
+            
+           
     });
-    $(newAnimalButton).on("click", function(){
-        
 
-  });
-
-});
-
-
-
-
-
+    });
 
 
 });
