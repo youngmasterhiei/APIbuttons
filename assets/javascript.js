fb6b32f5603d45db40681a45664608deab4e31c0
Hiei <james.illius@yahoo.com>
2018-06-01 22:39:05 -0400

    array buttons working, need to dry up code

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,23 +1,67 @@
-var animalArray = ["dog", "cat", "bear", "snake", "deer", "duck", "cow", "bull"];
+
 $(document).ready(function () {
 
     //needed to make a gloabl variable to avoid error at start. this is where the images and ratings are placed
     var animalDiv = $("<div>");
+    var animalArray = ["dog", "cat", "bear", "snake", "deer", "duck", "cow", "bull"];
 
 
 
 
 
-    createStartingAnimals();
 
-    function createStartingAnimals() {
-        for (var j = 0; j < animalArray.length; j++) {
-            AnimalButton = $("<button>" + animalArray[j] + "</button>");
-            AnimalButton.addClass("animalClass");
-            $("#newButtonArea").append(AnimalButton);
 
-        };
+
+    for (var j = 0; j < animalArray.length; j++) {
+        var animalButton = $("<button>" + animalArray[j] + "</button>");
+        animalButton.addClass("animalClass");
+        $("#newButtonArea").append(animalButton);
+
     };
+
+    $(".animalClass").on("click", function () {
+        var buttonSearch = $(this).text();
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + buttonSearch + "&api_key=I3LIxn3wdbBKTNOyOvfS4UCk3TvTUwrY";
+        console.log(buttonSearch);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            animalDiv.empty();
+            for (var i = 0; i < 25; i++) {
+                var displayStillImageUrl = response.data[i].images.fixed_height_small_still.url;
+                var displayGifImageUrl = response.data[i].images.fixed_height_small.url;
+                var rating = response.data[i].rating;
+                var image = $("<img>");
+                image.attr({ "src": displayStillImageUrl, "gif": displayGifImageUrl, "still": displayStillImageUrl });
+                //used to switch between still and gif image
+                image.addClass("still");
+                image.addClass("animalImageClass");
+                //switches between still image and gif
+                $(image).on("click", function () {
+
+                    if ($(this).hasClass("still")) {
+
+                        $(this).attr("src", $(this).attr("gif"));
+                        $(this).removeClass("still");
+
+                    }
+                    else {
+                        $(this).attr("src", $(this).attr("still"));
+                        $(this).addClass("still");
+                    }
+
+                });
+                // puts the div and images inside of an html div
+                $("#searchResultArea").append(animalDiv);
+                animalDiv.append("Rating:" + rating);
+                animalDiv.append(image);
+
+
+            };
+        });
+    });
+
 
 
 
@@ -97,4 +141,8 @@ $(document).ready(function () {
 
 
 });
+
+
+
+
 
